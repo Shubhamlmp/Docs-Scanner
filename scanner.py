cfbd6a8e1e9474a963e95b3956c8fd1355574351
Shubhamlmp <shubhunic@gmail.com>
2021-10-23 19:07:50 +0530

    loop over the contours
    Signed-off-by: Shubhamlmp <shubhunic@gmail.com>

--- scanner.py
+++ scanner.py
@@ -37,3 +37,15 @@ cv2.destroyAllWindows()
 cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 cnts = imutils.grab_contours(cnts)
 cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
+
+# loop over the contours
+for c in cnts:
+	# approximate the contour
+	peri = cv2.arcLength(c, True)
+	approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+
+	# if our approximated contour has four points, then we
+	# can assume that we have found our screen
+	if len(approx) == 4:
+		screenCnt = approx
+		break
