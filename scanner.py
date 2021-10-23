81e13d20f44729e54258963eeeaa45ae99ffaef0
Shubhamlmp <shubhunic@gmail.com>
2021-10-23 19:04:14 +0530

    show the original image and the edge detected image
    Signed-off-by: Shubhamlmp <shubhunic@gmail.com>

--- scanner.py
+++ scanner.py
@@ -24,3 +24,10 @@ image = imutils.resize(image, height = 500)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 gray = cv2.GaussianBlur(gray, (5, 5), 0)
 edged = cv2.Canny(gray, 75, 200)
+
+# show the original image and the edge detected image
+print("STEP 1: Edge Detection")
+cv2.imshow("Image", image)
+cv2.imshow("Edged", edged)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
