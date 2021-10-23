3ee8818930f4f256c1072c3b4e21ed93fd2da670
Shubhamlmp <shubhunic@gmail.com>
2021-10-23 18:57:59 +0530

    construct the argument parser and parse the arguments
    Signed-off-by: Shubhamlmp <shubhunic@gmail.com>

--- scanner.py
+++ scanner.py
@@ -5,3 +5,9 @@ import numpy as np
 import argparse
 import cv2
 import imutils
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required = True,
+	help = "Path to the image to be scanned")
+args = vars(ap.parse_args())
