d6e7353ebc07c4fda616a7fc59bc4e30684e696a
Shubhamlmp <shubhunic@gmail.com>
2021-10-23 18:56:28 +0530

    import necessary packages
    Signed-off-by: Shubhamlmp <shubhunic@gmail.com>

---
+++ scanner.py
@@ -0,0 +1,7 @@
+# import the necessary packages
+from transform import four_point_transform
+from skimage.filters import threshold_local
+import numpy as np
+import argparse
+import cv2
+import imutils
